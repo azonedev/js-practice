116c0cd542c27cbe02a26bf4297fe7b34557dcc6
azonedev <abdullah914115@gmail.com>
2021-01-26 00:09:47 +0600

    js auto semiclone

--- ES6/app.js
+++ ES6/app.js
@@ -1,4 +1,14 @@
-let color = ['red','black','brown'];
-let color_opt = ['white','light','blue'];
-let spreadArray = [...color,'success'];
-console.log(speadArray);
+// js can add auto-metic semiclone but not all 
+// ['hello','coders','here','we','are'].forEach(alert);
+
+// there will be an error for auto semiclone issue
+// alert("There will be an error")
+
+// [1, 2].forEach(alert)
+
+// there is no issue of auto semiclone like
+alert(
+    3+
+    2+
+    4
+)
