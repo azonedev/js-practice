98d1af89f48248aa1ebb3f9e2b49bb3d5b519f16
azonedev <abdullah914115@gmail.com>
2021-02-05 00:06:55 +0600

    Replacing If Else Chains with Switch

--- ES6/app.js
+++ ES6/app.js
@@ -1,28 +1,42 @@
-// Multiple Identical Options in Switch Statements
-function sequentialSizes(val) {
+//Replacing If Else Chains with Switch
+
+function chainToSwitch(val) {
     var answer = "";
     // Only change code below this line
   
+    if (val === "bob") {
+      answer = "Marley";
+    } else if (val === 42) {
+      answer = "The Answer";
+    } else if (val === 1) {
+      answer = "There is no #1";
+    } else if (val === 99) {
+      answer = "Missed me by this much!";
+    } else if (val === 7) {
+      answer = "Ate Nine";
+    }
+  
     switch(val){
+      case "bob":
+        return "Marley";
+      break;
+      case 42:
+        return "The Answer";
+      break;
       case 1:
-      case 2:
-      case 3:
-        return "Low";
-        break;
-      case 4:
-      case 5:
-      case 6:
-        return "Mid";
+        return "There is no #1";
+      break;
+      case 99:
+        return "Missed me by this much!";
       break;
       case 7:
-      case 8:
-      case 9:
-        return "High";
+        return "Ate Nine";
       break;
+  
     }
   
     // Only change code above this line
     return answer;
   }
   
-  sequentialSizes(1);
+  chainToSwitch(7);
