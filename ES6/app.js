58a2679bc4068daabb497206f0861e3277c02e9e
azonedev <abdullah914115@gmail.com>
2021-02-05 23:46:52 +0600

    access a object property with function

--- ES6/app.js
+++ ES6/app.js
@@ -1,34 +1,10 @@
-// create an object
-var developers = {
-    'frontend'   : 'Abdullah',
-    'backend'    : 'Mamun',
-    'full-stack' : 'Abdullah Al Mamun'
+// access a object property with function 
+function    accessObject(val){
+    var developers = {
+        'frontend'   : 'Abdullah',
+        'backend'    : 'Mamun',
+        'full-stack' : 'Abdullah Al Mamun'
+    }    
+    console.log(developers[val]); 
 }
-
-// access object with dot notation
-console.log(developers.frontend);
-console.log(developers["full-stack"]);
-
-// access object via bracket notation
-console.log(developers['frontend']);
-console.log(developers['backend']);
-
-// array inside an object property
-var object = {
-    'name':'Abdullah',
-    'legs':2,
-    'tails':5.6,
-    'friends':['Tushar','Sami','Rokibul']
-    };
-
-// change a object property
-object.name = "Abdullah Al Mamun";
-console.log(object);
-
-// add a new property on object
-object.age = 19;
-console.log(object);
-
-// delete properties from object
-delete object.tails;
-console.log(object);
+accessObject('full-stack');
