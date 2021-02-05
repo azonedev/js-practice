02f3f55cd6021505896fec6b7b6587b462cbcd75
azonedev <abdullah914115@gmail.com>
2021-02-05 16:49:47 +0600

    array inside an object property

--- ES6/app.js
+++ ES6/app.js
@@ -12,3 +12,11 @@ console.log(developers["full-stack"]);
 // access object via bracket notation
 console.log(developers['frontend']);
 console.log(developers['backend']);
+
+// array inside an object property
+var object = {
+    'name':'Abdullah',
+    'legs':2,
+    'tails':5.6,
+    'friends':['Tushar','Sami','Rokibul']
+    };
