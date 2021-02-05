94e4777f046b90770afc722f946fc91fc81dc905
azonedev <abdullah914115@gmail.com>
2021-02-05 16:40:04 +0600

    Create an object and access with dot & bracket notation

--- ES6/app.js
+++ ES6/app.js
@@ -1,3 +1,14 @@
-// prompt
-var a = prompt('Please Input you name','default_value');
-document.getElementById('root').innerHTML = `<h1>Hey my name is ${a} </h1>`;
+// create an object
+var developers = {
+    'frontend'   : 'Abdullah',
+    'backend'    : 'Mamun',
+    'full-stack' : 'Abdullah Al Mamun'
+}
+
+// access object with dot notation
+console.log(developers.frontend);
+console.log(developers["full-stack"]);
+
+// access object via bracket notation
+console.log(developers['frontend']);
+console.log(developers['backend']);
