61f1e31a8f8b9ad72f580e52547711b6a60ad9f1
azonedev <abdullah914115@gmail.com>
2022-08-01 00:24:09 +0600

    alert confirm prompt

---
+++ core/app.js
@@ -0,0 +1,4 @@
+alert('alert is use to alert message');
+console.log('console is use to print message');
+confirm('confirm is use to ask user to confirm');
+prompt('prompt is use to ask user to enter value');
