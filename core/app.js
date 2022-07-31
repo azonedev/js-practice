e9f01fff9c805a7ec941d081ff8d7df01ee94a83
azonedev <abdullah914115@gmail.com>
2022-08-01 00:33:28 +0600

    modified:   core/app.js ternary operator

--- core/app.js
+++ core/app.js
@@ -2,3 +2,7 @@ alert('alert is use to alert message');
 console.log('console is use to print message');
 confirm('confirm is use to ask user to confirm');
 prompt('prompt is use to ask user to enter value');
+
+
+// ternary operator
+condition ? true : false 
